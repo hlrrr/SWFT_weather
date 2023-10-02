b56ef8b6e60a60c9947cf36762179dd970af7227
hlrrr <twfvtn@gmail.com>
2023-10-02 19:48:03 +0900

    Initial Commit

---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by hylee on 2023/10/02.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
